f020891eb5fdb5019fe6181c70a78b1814643b44
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-04-05 19:33:23 +0700

    Design Database Schema & Manage MongoDB Connection.

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,30 +1,29 @@
 import { MongoClient } from 'mongodb'
 import { env } from '*/config/environment'
 
+let dbInstance = null;
+
 export const connectDB = async () => {
     const client = new MongoClient(env.MONGODB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     })
 
-    try {
-        // Connect the client to the server
-        await client.connect()
+    await client.connect()
 
-        // List databases
-        await listDatabases(client)
-
-        console.log('Connected successfully to server!')
-    } finally {
-        // Ensures that the client will close when finish/error
-        await client.close()
-        console.log('closed')
-    }
+    // Assign clientDB to our DBInstance
+    dbInstance = client.db(env.DATABASE_NAME);
 }
 
-const listDatabases = async (client) => {
-    const databaseList = await client.db().admin().listDatabases()
-    console.log(databaseList)
-    console.log('Your databases:')
-    databaseList.databases.forEach(db => console.log(` - ${db.name}`))
+// Get Database Instance
+export const getDB = () => {
+    if (!dbInstance) throw new Error('Must connect to Database first!')
+    return dbInstance
 }
+
+// const listDatabases = async (client) => {
+//     const databaseList = await client.db().admin().listDatabases()
+//     console.log(databaseList)
+//     console.log('Your databases:')
+//     databaseList.databases.forEach(db => console.log(` - ${db.name}`))
+// }
