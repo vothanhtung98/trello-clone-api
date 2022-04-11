d0d7cc1305ccf326bd1f3d5ea797adb752176ad7
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-04-11 08:15:11 +0700

    Combine Apis For Drag & Drop.

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -12,4 +12,19 @@ const createNew = async (req, res) => {
     }
 }
 
-export const CardController = { createNew }
+const update = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await CardService.update(id, req.body)
+        res.status(HttpStatusCode.OK).json(result)
+    } catch (error) {
+        res.status(HttpStatusCode.INTERNAL_SERVER).json({
+            error: error.message
+        })
+    }
+}
+
+export const CardController = {
+    createNew,
+    update
+}
