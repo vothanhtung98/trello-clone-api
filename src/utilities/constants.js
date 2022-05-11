a70049c0854d37c1bbb8f707f1b048a63532df3b
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-05-11 13:31:38 +0700

    Redeploy.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -9,5 +9,6 @@ export const HttpStatusCode = {
 export const WHITELIST_DOMAINS = [
     'http://localhost:3000',
     'https://trello-clone-web-bbf04.web.app',
-    'https://my-trello-vothanhtung.web.app'
+    'https://my-trello-vothanhtung.web.app',
+    'https://my-trello-vothanhtung.firebaseapp.com/'
 ]
